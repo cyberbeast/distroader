eaba27420a366ed1531a510502159704bf4ab455
cyberbeast <sandeshgade@gmail.com>
2016-02-16 00:10:14 +0530

    Aesthetic fixes and modularization for worker setup function!

--- setup.sh
+++ setup.sh
@@ -12,20 +12,12 @@ echo -e "
 ░       ░ ░           ░                       ░                                   
 "
 
-#Check for superuser privileges and branch accordingly!
-if (( $EUID != 0 )); then
-	print $WARNING "Script is called without superuser privileges!";
-	print $INFO "Script will now attempt to enter superuser mode!";
-	# Execute setup script with superuser access.
-	sudo ./setup.sh;
-else
+function worker_setup() {
 	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "-";
-	echo -e "      _ _            _   
-  ___| (_) ___ _ __ | |_ 
- / __| | |/ _ \ '_ \| __|
-| (__| | |  __/ | | | |_ 
- \___|_|_|\___|_| |_|\__|
-                         
+	echo -e "
+┬ ┬┌─┐┬─┐┬┌─┌─┐┬─┐  ┌─┐┌─┐┌┬┐┬ ┬┌─┐
+││││ │├┬┘├┴┐├┤ ├┬┘  └─┐├┤  │ │ │├─┘
+└┴┘└─┘┴└─┴ ┴└─┘┴└─  └─┘└─┘ ┴ └─┘┴               
 "
 	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "-";
 	printf "Please be patient for the entire duration of the script. cyberbeast's minions are putting together a lot of things together! \n \n"
@@ -66,6 +58,78 @@ else
 	esac
 	printf ": DONE! \n"
 
-	
+	printf "re"
+	python3 -m re &> /dev/null;
+	check_status_code=$?;
+	case "$check_status_code" in
+		0)	printf " : detected! ";
+			;;
+		*)	printf " : not found. Installing NOW! ";
+			sudo pip3 install re;
+			;;
+	esac
+	printf ": DONE! \n"
+
+	printf "urllib"
+	python3 -m urllib &> /dev/null;
+	check_status_code=$?;
+	case "$check_status_code" in
+		0)	printf " : detected! ";
+			;;
+		*)	printf " : not found. Installing NOW! ";
+			sudo pip3 install urllib;
+			;;
+	esac
+	printf ": DONE! \n"
+
+	printf "csv"
+	python3 -m csv &> /dev/null;
+	check_status_code=$?;
+	case "$check_status_code" in
+		0)	printf " : detected! ";
+			;;
+		*)	printf " : not found. Installing NOW! ";
+			sudo pip3 install csv;
+			;;
+	esac
+	printf ": DONE! \n"
+
+	printf "os"
+	python3 -m os &> /dev/null;
+	check_status_code=$?;
+	case "$check_status_code" in
+		0)	printf " : detected! ";
+			;;
+		*)	printf " : not found. Installing NOW! ";
+			sudo pip3 install os;
+			;;
+	esac
+	printf ": DONE! \n"
+
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "-";
+}
+
+#Check for superuser privileges and branch accordingly!
+if (( $EUID != 0 )); then
+	print $WARNING "Script is called without superuser privileges!";
+	print $INFO "Script will now attempt to enter superuser mode!";
+	# Execute setup script with superuser access.
+	sudo ./setup.sh;
+else
+	echo -e "
+██╗    ██╗        ██╗    ███████╗
+██║    ██║       ██╔╝    ██╔════╝
+██║ █╗ ██║      ██╔╝     ███████╗
+██║███╗██║     ██╔╝      ╚════██║
+╚███╔███╔╝    ██╔╝       ███████║
+ ╚══╝╚══╝     ╚═╝        ╚══════╝
+                                 
+	"
+	printf "Choose responsibly! W(orker) or S(erver)!: ";	
+	select response in "W" "S"; do
+    case $response in
+        W ) worker_setup;;
+        S ) server_setup;;
+    esac
 fi
 
